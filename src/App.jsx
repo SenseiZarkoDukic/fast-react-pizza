083c57da956a5c9896cd8e0549c139d74c955445
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-16 18:26:47 +0100

    Import createBrowserRouter to App.jsx file, and create path and elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./menu/Menu.jsx";
+const BrowserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
 function App() {
   return <div>Hello Vite!</div>;
 }
