7299b9e933b65719487780a625addb730bcd75fd
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-18 04:28:13 +0100

    Create SearchOrder component

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      type="text"
+      placeholder="Search order #"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+export default SearchOrder;
