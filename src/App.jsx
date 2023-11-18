99172177a79f8782a2f0aa9958167559bb05d3f9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-18 01:34:07 +0100

    Create errorElement using Error component in the parent router element AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Home from "./ui/Home.jsx";
+import Error from "./ui/Error.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
@@ -9,6 +11,7 @@ import AppLayout from "./ui/AppLayout.jsx";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
