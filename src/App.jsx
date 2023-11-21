42d5010142d5ce76a1a45ffc66ea98eca4360274
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-21 16:13:35 +0100

    Add errorElement to Order route

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
