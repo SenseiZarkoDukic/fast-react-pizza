19b7da5a880df89de5813a3550a5389251d4f54b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-21 17:24:54 +0100

    Clean code, import useLoaderData

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,12 +1,12 @@
 // Test ID: IIDSAT
 
-import { useParams } from "react-router-dom";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 import { getOrder } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
 
 const order = {
   id: "ABCDEF",
@@ -44,6 +44,7 @@ const order = {
 };
 
 function Order() {
+  const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
