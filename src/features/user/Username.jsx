ea1f0e9fc1da59fc423f20986702061b8f1f940d
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-23 17:41:32 +0100

    Add class hidden to have display:none of Username element

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Zarko</div>;
+  return <div className="hidden text-sm font-semibold">Zarko</div>;
 }
 
 export default Username;
