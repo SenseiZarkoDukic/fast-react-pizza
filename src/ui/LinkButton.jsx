9436376aa54dfbcfc3d85a707245e21cb2e409b9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-27 18:13:34 +0100

    Add LinkButton to Error component instead of button

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,6 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+
+  if (to === -1)
+    return <button onClick={() => navigate(-1)}>{children}</button>;
+
   return (
     <Link
       to={to}
