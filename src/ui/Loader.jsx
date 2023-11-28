0c4853196ad450b3c8baf8449e6884dcd6724797
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-28 15:57:16 +0100

    Stylize Loader using absolute positioning

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="bg:slate-200/20 absolute inset-0 flex w-screen items-center  justify-center backdrop-blur-sm ">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
