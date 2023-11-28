f115be61ac9f31aa8ce1c327c1267195a1bc1e4c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-28 18:56:58 +0100

    Stylize ul in the Cart component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -36,13 +36,13 @@ function Cart() {
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <ul>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
 
-      <div>
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
