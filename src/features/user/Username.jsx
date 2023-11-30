43a860008ec29e21bf17c404b65a557586bcc99f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-30 17:00:54 +0100

    Create username variable using useSelector in the Username component

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from 'react-redux';
+import store from '../../store';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Zarko</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) {
+    return null;
+  }
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
