cb64c0855b9900c852b17a204ba3897cfff7b3c6
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-30 17:31:00 +0100

    Set username using redux in CreateUser, setting global state

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import store from '../../store';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) {
+      return;
+    }
+
+    dispatch(updateName(username));
+    navigate('/menu');
+    // setUsername('');
   }
 
   return (
