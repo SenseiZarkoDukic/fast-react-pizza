1cf6f7e9ad588ecc679105836076e56b0999dcfd
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-30 18:49:39 +0100

    Create cartSlice redux with reducers for cart component

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  // cart: [],
+
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action) => {},
+    deleteItem: (state, action) => {},
+    increaseItemQuantity: (state, action) => {},
+    decreaseItemQuantity: (state, action) => {},
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
