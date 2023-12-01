2bdeda50512532dc03b6d5a2c86299876b3b374c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-01 18:16:10 +0100

    Add functionality to the Button Add to cart, condition it with !soldOut

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,6 +4,8 @@ import { formatCurrency } from '../../utils/helpers';
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
+  function handleAddToCart() {}
+
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -24,7 +26,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
