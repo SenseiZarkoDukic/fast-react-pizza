f70b0719d334309a5f321f21b9aa224c947ef16c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-05 18:59:21 +0100

    Use store.dispatch clearCart action with the clearCart function in CreateOrder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,8 +3,9 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
-import { getCart } from '../cart/cartSlice';
+import { clearCart, getCart } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -131,6 +132,8 @@ export async function action({ request }) {
   // If everything is okey, create new order and redirect
   const newOrder = await createOrder(order);
 
+  // Do NOT overuse
+  store.dispatch(clearCart());
   return redirect(`/order/${newOrder.id}`);
 }
 
